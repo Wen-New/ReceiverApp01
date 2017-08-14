5d0da0f8b0e18fc04deaf5d5f9bdc4544c334550
Wen-New <16022742@myrp.edu.sg>
2017-08-14 13:59:53 +0800

    With SMS

--- app/src/main/java/sg/edu/rp/c346/receiverapp/BatteryReceiver.java
+++ app/src/main/java/sg/edu/rp/c346/receiverapp/BatteryReceiver.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.receiverapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsManager;
 import android.widget.Toast;
 
 public class BatteryReceiver extends BroadcastReceiver{
@@ -11,5 +12,8 @@ public class BatteryReceiver extends BroadcastReceiver{
     public void onReceive(Context context, Intent intent) {
         Toast my_toast = Toast.makeText(context, "Low Battery", Toast.LENGTH_LONG);
         my_toast.show();
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("5554", null, "Hello World", null, null);
     }
 }
